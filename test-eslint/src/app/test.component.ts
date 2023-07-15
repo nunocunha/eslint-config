fa50536bf78f156e73446aeea43b708abc73bff3
Nuno Cunha <me@nunocunha.pt>
2023-07-15 12:46:53 +0100

    Add test Angular template

--- test-eslint/src/app/test.component.ts
+++ test-eslint/src/app/test.component.ts
@@ -1,3 +1,12 @@
+import {CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  selector: `app-root`,
+  templateUrl: `./test.component.html`,
+})
 export class TestComponent {
   public get x(): string {
     return `x`;
