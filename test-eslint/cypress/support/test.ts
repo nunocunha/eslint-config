29e2cfca20486baded33278f475bd21f7824bb77
Nuno Cunha <me@nunocunha.pt>
2023-07-15 22:33:31 +0100

    Add case on test code

--- test-eslint/cypress/support/test.ts
+++ test-eslint/cypress/support/test.ts
@@ -5,3 +5,8 @@ export const BASE_URL = `base://url`;
 export type Generic<T, K extends keyof T> = { [P in K]: T[P]; } & Partial<T>;
 
 export class Class {}
+
+export enum Unit {
+  QUANTITY,
+  PART_PER_UNIT
+}
